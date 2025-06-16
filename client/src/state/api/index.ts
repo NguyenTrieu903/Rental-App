303600b88db7d18e2d65be45361b680eee4684ba
TrieuPorches <80805409+NguyenTrieu903@users.noreply.github.com>
2025-06-16 20:39:47 +0700

    refactor code

--- client/src/state/api/index.ts
+++
@@ -1,5 +0,0 @@
-// Export the base API and all hooks
-// export { baseApi } from "./baseApi";
-// export { useGetAuthUserQuery } from "./authApi";
-// export { useUpdateTenantSettingsMutation } from "./tenantApi";
-// export { useUpdateManagerSettingsMutation } from "./managerApi";
